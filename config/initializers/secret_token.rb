fb083f97e7229f76e6db39c1433dcc1b4e6fa10c
Axel Samuelsson <limbero@limbero.org>
2013-06-09 21:11:52 +0200

    First things first I guess

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Msgr::Application.config.secret_token = '2adda9339358051f1eec846262e6eade376c1daeb36f0aaa5920e41f1a40dc86f554262a1c300ca09e02a7612c841ce1520968f2a2b687d55fa8d6b326ddb909'
